dd96accca8a5dc2e0e254a19dfe56d8103984410
pyush-18 <piyushjoshi1812@gmail.com>
2025-12-24 18:34:42 +0530

    still fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(), 
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, 
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
+        manualChunks: undefined,
       },
     },
-    target: 'esnext', 
-    reportCompressedSize: false, 
   },
 });
