15b95b20362ef7963ad723b8b317810ff553ab10
pyush-18 <piyushjoshi1812@gmail.com>
2025-12-24 18:31:16 +0530

    fixing performance

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+  ],
   build: {
     outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true,
       },
     },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    target: 'esnext', 
+    reportCompressedSize: false, 
   },
 });
