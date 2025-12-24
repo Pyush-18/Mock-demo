e95e3c173f102d0c934c1b3d4a9615b29789f6e4
pyush-18 <piyushjoshi1812@gmail.com>
2025-12-24 19:14:47 +0530

    fix double load razorpay script

--- client/src/componets/subscription-model/Pricing.jsx
+++ client/src/componets/subscription-model/Pricing.jsx
@@ -13,6 +13,10 @@ import { db } from "../../config/firebase";
 
 const loadRazorpayScript = () => {
   return new Promise((resolve) => {
+    if (window.Razorpay) {
+      resolve(true);
+      return;
+    }
     const script = document.createElement("script");
     script.src = "https://checkout.razorpay.com/v1/checkout.js";
     script.async = true;
